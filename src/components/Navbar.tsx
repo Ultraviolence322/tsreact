17d1f5cdb8491f5ae831af5b86d76023e5afe9d9
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-09 10:20:01 +0300

    added NavLinnk to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import React from "react"
+import {NavLink} from "react-router-dom"
 
 export const Navbar: React.FC = () => {
 	return (
 		<ul>
-			<li>Main</li>
-			<li>List</li>
-			<li>New</li>
+			<li><NavLink to="/">Main</NavLink></li>
+			<li><NavLink to="/about">About</NavLink></li>
 		</ul>
 	)
 }
