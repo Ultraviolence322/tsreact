21f98e8d7edce6b33eea77adafd7559c19d311f8
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-09 09:32:59 +0300

    added components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { TodoForm } from "./components/TodoForm";
+import { ITodo } from "./interfaces/Todo";
+import {TodoList} from './components/TodoList'
+
+import './index.css'
 
 function App() {
+  const [list, setList] = useState<ITodo[]>([])
+
+  const addHandler = (value: string): void => {
+    const newTodo: ITodo = {
+      id: Date.now(),
+      title: value,
+      completed: false
+    }
+
+    setList([newTodo, ...list])
+  }
+
+  const changeCompleteHandler = (id: number) => {
+    const newList = list.map((todo): ITodo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed 
+      } 
+      return todo
+    })
+
+    setList(newList)
+  }
+
+  const removeTodoHandler = (id: number): void => {
+    const newList = list.filter((todo) => todo.id !== id)
+
+    setList(newList)
+  }
+
   return (
-    <h1>HI TSX</h1>
+    <>
+      <Navbar />
+      <h1>HI TSX</h1>
+      <TodoForm addHandler={addHandler} />
+      <TodoList 
+        removeTodoHandler={removeTodoHandler} 
+        changeCompleteHandler={changeCompleteHandler} 
+        list={list}
+      />
+    </>
   );
 }
 
